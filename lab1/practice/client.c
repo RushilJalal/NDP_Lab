d991e98b4270765ee8fc3e153cb73399b7a6808a
rushil <rushiljalal@gmail.com>
2025-01-06 19:49:52 +0530

    format and move code

--- lab1/practice/client.c
+++ lab1/practice/client.c
@@ -1,9 +1,9 @@
-#include<stdio.h>
-#include<unistd.h>
-#include<sys/socket.h>
-#include<sys/types.h>
-#include<netinet/in.h>
-#include<string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <string.h>
 #define MAXSIZE 50
 
 int main()
@@ -16,31 +16,35 @@ int main()
     // Create client socket
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-    if (sockfd == -1) {
+    if (sockfd == -1)
+    {
         printf("\nSocket Creation Error\n");
         return 1;
     }
 
     serveraddr.sin_family = AF_INET;
     serveraddr.sin_port = htons(3388);
-    serveraddr.sin_addr.s_addr = inet_addr("127.0.0.1");  // Server IP address
+    serveraddr.sin_addr.s_addr = inet_addr("127.0.0.1"); // Server IP address
 
     // Connect to the server
-    retval = connect(sockfd, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
+    retval = connect(sockfd, (struct sockaddr *)&serveraddr, sizeof(serveraddr));
 
-    if (retval == -1) {
+    if (retval == -1)
+    {
         printf("Connection error\n");
         return 1;
     }
 
     // Continuously prompt for user input and send messages to the server
-    while (1) {
+    while (1)
+    {
         printf("Enter message: ");
-        scanf("%49s", buff);  // Prevent buffer overflow by limiting input size
+        scanf("%49s", buff); // Prevent buffer overflow by limiting input size
 
         // Send the message to the server
-        sentbytes = send(sockfd, buff, strlen(buff) + 1, 0);  // Send actual string length
-        if (sentbytes == -1) {
+        sentbytes = send(sockfd, buff, strlen(buff) + 1, 0); // Send actual string length
+        if (sentbytes == -1)
+        {
             printf("Send error\n");
             close(sockfd);
             return 1;
--- lab1/practice/server.c
+++ lab1/practice/server.c
@@ -1,10 +1,10 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<unistd.h>
-#include<sys/socket.h>
-#include<sys/types.h>
-#include<netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
 #define MAXSIZE 90
 
 int main()
@@ -14,11 +14,13 @@ int main()
     int recvdbytes;
     struct sockaddr_in serveraddr, clientaddr;
     char buff[MAXSIZE];
-    
+
     // Create server socket
+    // will store 3 since 0,1,2 are reserved
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-    if (sockfd == -1) {
+    if (sockfd == -1)
+    {
         printf("\nSocket creation error\n");
         return 1;
     }
@@ -31,8 +33,9 @@ int main()
     serveraddr.sin_addr.s_addr = INADDR_ANY;
 
     // Bind the server socket to the address
-    retval = bind(sockfd, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
-    if (retval == -1) {
+    retval = bind(sockfd, (struct sockaddr *)&serveraddr, sizeof(serveraddr));
+    if (retval == -1)
+    {
         printf("Binding error\n");
         close(sockfd);
         return 1;
@@ -40,16 +43,18 @@ int main()
 
     // Listen for incoming connections
     retval = listen(sockfd, 1);
-    if (retval == -1) {
+    if (retval == -1)
+    {
         printf("Listen error\n");
         close(sockfd);
         return 1;
     }
 
     actuallen = sizeof(clientaddr);
-    newsockfd = accept(sockfd, (struct sockaddr*)&clientaddr, &actuallen);
+    newsockfd = accept(sockfd, (struct sockaddr *)&clientaddr, &actuallen);
 
-    if (newsockfd == -1) {
+    if (newsockfd == -1)
+    {
         printf("Accept error\n");
         close(sockfd);
         return 1;
@@ -58,14 +63,16 @@ int main()
     printf("Client connected!\n");
 
     // Continuously receive and display messages from client
-    while (1) {
-        recvdbytes = recv(newsockfd, buff, sizeof(buff) - 1, 0);  // Receive up to the buffer size
-        if (recvdbytes == -1) {
+    while (1)
+    {
+        recvdbytes = recv(newsockfd, buff, sizeof(buff) - 1, 0); // Receive up to the buffer size
+        if (recvdbytes == -1)
+        {
             printf("Receive error\n");
             break;
         }
 
-        buff[recvdbytes] = '\0';  // Null-terminate the received string
+        buff[recvdbytes] = '\0'; // Null-terminate the received string
         printf("Received from client: %s\n", buff);
     }
 
