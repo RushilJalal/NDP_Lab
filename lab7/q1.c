cc19836740ecb9bd5087f153ca35cfe44c20bfe8
rushil <rushiljalal@gmail.com>
2025-04-06 12:20:15 +0530

    latest

--- lab7/q1.c
+++ lab7/q1.c
@@ -1,33 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 
 #define BUCKET_SIZE 10
 #define PACKET_SIZE 4
 #define OUTGOING_RATE 1
 
-void leaky_bucket(int* packet_times, int num_packets) {
+void leaky_bucket(int *packet_times, int num_packets)
+{
     int bucket = 0;
     int outgoing_time = 0;
     int conforming = 0, nonconforming = 0;
-    
-    for (int i = 0; i < num_packets; i++) {
+
+    for (int i = 0; i < num_packets; i++)
+    {
         int arrival_time = packet_times[i];
 
-        
-        while (outgoing_time < arrival_time) {
-            if (bucket > 0) {
+        while (outgoing_time < arrival_time)
+        {
+            if (bucket > 0)
+            {
                 bucket--;
                 printf("Time %d: Drained 1 byte from bucket. Bucket size = %d bytes\n", outgoing_time, bucket);
             }
             outgoing_time++;
         }
 
-        if (bucket + PACKET_SIZE <= BUCKET_SIZE) {
+        if (bucket + PACKET_SIZE <= BUCKET_SIZE)
+        {
             bucket += PACKET_SIZE;
             conforming++;
             printf("Time %d: Conforming packet of size %d bytes added to bucket. Bucket size = %d bytes\n", arrival_time, PACKET_SIZE, bucket);
-        } else {
+        }
+        else
+        {
             nonconforming++;
             printf("Time %d: Nonconforming packet of size %d bytes dropped (Bucket Overflow). Bucket size = %d bytes\n", arrival_time, PACKET_SIZE, bucket);
         }
@@ -38,7 +43,8 @@ void leaky_bucket(int* packet_times, int num_packets) {
     printf("Nonconforming packets: %d\n", nonconforming);
 }
 
-int main() {
+int main()
+{
     int packet_times[] = {1, 2, 3, 5, 6, 8, 11, 12, 15, 16, 19};
     int num_packets = sizeof(packet_times) / sizeof(packet_times[0]);
 
--- lab7/q2.c
+++ lab7/q2.c
@@ -1,30 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 
 #define REPLENISH_RATE 10
 #define BUCKET_SIZE 50
 #define PACKET_SIZE 15
 #define SEND_INTERVAL 0.5
 
-void token_bucket_simulation_1() {
+void token_bucket_simulation_1()
+{
     int tokens = BUCKET_SIZE;
     double time = 0.0;
     int packets_sent = 0;
 
-    while (time <= 1.5) {
+    while (time <= 1.5)
+    {
         tokens += REPLENISH_RATE * 0.5;
-        if (tokens > BUCKET_SIZE) {
+        if (tokens > BUCKET_SIZE)
+        {
             tokens = BUCKET_SIZE;
         }
 
         printf("Time %.1f sec: Tokens in bucket = %d KB\n", time, tokens);
 
-        if ((time > 0) && (time - (packets_sent * SEND_INTERVAL) >= SEND_INTERVAL)) {
-            if (tokens >= PACKET_SIZE) {
+        if ((time > 0) && (time - (packets_sent * SEND_INTERVAL) >= SEND_INTERVAL))
+        {
+            if (tokens >= PACKET_SIZE)
+            {
                 tokens -= PACKET_SIZE;
                 printf("Packet of 15 KB sent at time %.1f sec. Tokens left: %d KB\n", time, tokens);
-            } else {
+            }
+            else
+            {
                 printf("Packet of 15 KB queued or dropped at time %.1f sec. Tokens left: %d KB\n", time, tokens);
             }
             packets_sent++;
@@ -36,24 +42,31 @@ void token_bucket_simulation_1() {
     printf("%d tokens left after 1.5 seconds\n", tokens);
 }
 
-void token_bucket_simulation_2() {
+void token_bucket_simulation_2()
+{
     int tokens = BUCKET_SIZE;
     double time = 0.0;
     int packets_sent = 0;
 
-    while (1) {
+    while (1)
+    {
         tokens += REPLENISH_RATE * 0.5;
-        if (tokens > BUCKET_SIZE) {
+        if (tokens > BUCKET_SIZE)
+        {
             tokens = BUCKET_SIZE;
         }
 
         printf("Time %.1f sec: Tokens in bucket = %d KB\n", time, tokens);
 
-        if ((time > 0) && (time - (packets_sent * SEND_INTERVAL) >= SEND_INTERVAL)) {
-            if (tokens >= PACKET_SIZE) {
+        if ((time > 0) && (time - (packets_sent * SEND_INTERVAL) >= SEND_INTERVAL))
+        {
+            if (tokens >= PACKET_SIZE)
+            {
                 tokens -= PACKET_SIZE;
                 printf("Packet of 15 KB sent at time %.1f sec. Tokens left: %d KB\n", time, tokens);
-            } else {
+            }
+            else
+            {
                 printf("Packet of 15 KB queued or dropped at time %.1f sec. Tokens left: %d KB\n", time, tokens);
                 break;
             }
@@ -67,11 +80,11 @@ void token_bucket_simulation_2() {
     printf("Time till packets are queued or dropped, %.lf\n", time);
 }
 
-int main() {
+int main()
+{
     token_bucket_simulation_1();
 
     token_bucket_simulation_2();
-
 
     // Maximum possible burst size with max rate R = 20 KBps (rate limit) is 50 KB.
     // This can be inferred from the bucket size (as the token bucket limits the burst size).
